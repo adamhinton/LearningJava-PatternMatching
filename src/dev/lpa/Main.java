32dc807b82970835f5c43dd145a65ce6cb02b76f
adamd <adam.d.hinton@gmail.com>
2024-04-23 01:41:46 -0400

    matching and pattern methods

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -16,6 +16,11 @@ public class Main {
         Pattern firstPattern = Pattern.compile("[A-Z].*[.]");
         var matcher = firstPattern.matcher(sentence);
         System.out.println(matcher.matches() + ": " + sentence);
+        System.out.println("sentence.length: " + sentence.length());
+        System.out.println("Matched Ending Index: " + matcher.end());
+
+        System.out.println(matcher.lookingAt() + ": " + sentence);
+        System.out.println("Matched Ending Index: " + matcher.end());
 
 
 
