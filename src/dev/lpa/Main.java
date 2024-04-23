3528a99d044a9965687385ad358fbdbd19840b03
adamd <adam.d.hinton@gmail.com>
2024-04-23 01:38:34 -0400

    learning about compiling regexes

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String sentence = "I like motorcycles.";
+
+        // Starts with capital letter, then any characters, then period
+        // Regex is compiled in to a Pattern by Java's regex processor
+        boolean matched = Pattern.matches("[A-Z].*[.]", sentence);
+        System.out.println(matched + ": " + sentence);
+
+        // Much more efficient for scaling
+        Pattern firstPattern = Pattern.compile("[A-Z].*[.]");
+        var matcher = firstPattern.matcher(sentence);
+        System.out.println(matcher.matches() + ": " + sentence);
+
+
+
     }
 }
